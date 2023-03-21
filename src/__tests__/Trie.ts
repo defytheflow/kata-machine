a5b104d90617706fbb932add4e346ed232163a62
Artyom Danilov <defytheflow@gmail.com>
2023-03-21 14:39:46 +0300

    feat: complete Trie

--- src/__tests__/Trie.ts
+++ src/__tests__/Trie.ts
@@ -1,23 +1,39 @@
 import Trie from "@code/Trie";
 
-test("Trie", function() {
-    const trie = new Trie();
-    trie.insert("foo");
-    trie.insert("fool");
-    trie.insert("foolish");
-    trie.insert("bar");
+// TODO: handle ascii characters and upper case letters
+test("Trie", function () {
+  const trie = new Trie();
 
-    expect(trie.find("fo").sort()).toEqual([
-        "foo",
-        "fool",
-        "foolish",
-    ]);
+  trie.insert("foo");
+  trie.insert("fool");
+  trie.insert("foolish");
+  trie.insert("bar");
 
-    trie.delete("fool");
+  expect(trie.find("fo").sort()).toEqual(["foo", "fool", "foolish"]);
 
-    expect(trie.find("fo").sort()).toEqual([
-        "foo",
-        "foolish",
-    ]);
+  trie.delete("fool");
+
+  expect(trie.find("fo").sort()).toEqual(["foo", "foolish"]);
 });
 
+test("My Trie (deletes empty nodes without children)", function () {
+  const trie = new Trie();
+
+  trie.insert("fool");
+  trie.insert("foolish");
+  trie.insert("foolishness");
+
+  expect(trie.find("fool").sort()).toEqual(["fool", "foolish", "foolishness"]);
+
+  trie.delete("foolish");
+
+  expect(trie.toArray().length).toEqual("foolishness".length + 1);
+
+  trie.delete("foolishness");
+
+  expect(trie.toArray().length).toEqual("fool".length + 1);
+
+  trie.delete("fool");
+
+  expect(trie.toArray().length).toEqual(1);
+});
---
+++ src/day1/Trie.ts
@@ -0,0 +1,118 @@
+const ALPHABET_SIZE = 26;
+
+class TrieNode {
+  value: string;
+  children: (TrieNode | undefined)[] = Array.from({ length: ALPHABET_SIZE });
+  isEndOfWord = false;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  hasChildren(): boolean {
+    return this.children.some(Boolean);
+  }
+}
+
+// TODO: handle ascii characters and upper case letters
+export default class Trie {
+  private root = new TrieNode("");
+
+  toArray(): string[] {
+    return (function traverse(node: TrieNode, nodes: string[]) {
+      nodes.push(node.value);
+
+      for (let child of node.children) {
+        if (child) {
+          traverse(child, nodes);
+        }
+      }
+
+      return nodes;
+    })(this.root, []);
+  }
+
+  insert(word: string): void {
+    var curr = this.root;
+
+    for (let char of word) {
+      let idx = Trie.getIdx(char);
+      let node = curr.children[idx];
+      if (!node) {
+        node = curr.children[idx] = new TrieNode(char);
+      }
+      curr = node;
+    }
+
+    curr.isEndOfWord = true;
+  }
+
+  delete(word: string): boolean {
+    var curr = this.root;
+    var nodes = [curr];
+
+    for (let char of word) {
+      let idx = Trie.getIdx(char);
+      let node = curr.children[idx];
+      if (!node) {
+        return false;
+      }
+      nodes.push(node);
+      curr = node;
+    }
+
+    if (!curr.isEndOfWord) {
+      return false;
+    }
+
+    curr.isEndOfWord = false;
+
+    for (let i = nodes.length - 2; i >= 0; i--) {
+      let parent = nodes[i];
+      let child = nodes[i + 1];
+      if (child.isEndOfWord || child.hasChildren()) {
+        break;
+      }
+      let idx = Trie.getIdx(child.value);
+      parent.children[idx] = undefined;
+    }
+
+    return true;
+  }
+
+  find(prefix: string): string[] {
+    var curr = this.root;
+
+    for (let char of prefix) {
+      let idx = Trie.getIdx(char);
+      let node = curr.children[idx];
+      if (!node) {
+        return [];
+      }
+      curr = node;
+    }
+
+    return (function traverse(
+      node: TrieNode,
+      prefix: string,
+      results: string[],
+    ) {
+      if (node.isEndOfWord) {
+        results.push(prefix);
+      }
+
+      for (let child of node.children) {
+        if (child) {
+          traverse(child, prefix + child.value, results);
+        }
+      }
+
+      return results;
+    })(curr, prefix, []);
+  }
+
+  private static getIdx(ch: string): number {
+    const ZERO = "a".charCodeAt(0);
+    return ch.toLowerCase().charCodeAt(0) - ZERO;
+  }
+}
